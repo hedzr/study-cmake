250b716f1394187ea584ee192c38d1e717721931
Hedzr Yeh <hedzrz@gmail.com>
2020-11-24 08:42:52 +0800

    added github actions script

---
+++ cmake/prerequisites.cmake
@@ -0,0 +1,9 @@
+
+#cmake_minimum_required(VERSION 3.13)
+#mark_as_advanced(CMAKE_BACKWARDS_COMPATIBILITY)
+# saner control structure syntax
+#set(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS true)
+
+
+#list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/${CMAKE_SCRIPTS}/Modules:${CMAKE_SOURCE_DIR}/${CMAKE_SCRIPTS}")
+#message("CMAKE_MODULE_PATH = ${CMAKE_MODULE_PATH}") ###
