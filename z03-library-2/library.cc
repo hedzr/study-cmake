40195c9b4444a0c144f7d360d3b910b75f6b2457
Hedzr Yeh <hedzrz@gmail.com>
2020-11-24 08:39:10 +0800

    c++ code for the-compiler.h

--- z03-library-2/library.cc
+++ z03-library-2/library.cc
@@ -5,6 +5,46 @@
 #include "mucht/mucht.hh"
 #include <iostream>
 
+#include "the-compiler.h"
+
+#if The_COMPILER_IS_AppleClang
+#define COMPILER 1
+#endif
+
+#if The_COMPILER_IS_GNU
+#define COMPILER 2
+#endif
+
+#if The_COMPILER_CXX_VARIADIC_TEMPLATES
+template<int I, int... Is>
+struct Interface;
+
+template<int I>
+struct Interface<I>
+{
+  static int accumulate()
+  {
+    return I;
+  }
+};
+
+template<int I, int... Is>
+struct Interface
+{
+  static int accumulate()
+  {
+    return I + Interface<Is...>::accumulate();
+  }
+};
+#else
+template<int I1, int I2 = 0, int I3 = 0, int I4 = 0>
+struct Interface
+{
+    static int accumulate() { return I1 + I2 + I3 + I4; }
+};
+#endif
+
+
 void hello(std::string const &name) {
     std::cout << "Hello, " << name << "!\n";
 }
